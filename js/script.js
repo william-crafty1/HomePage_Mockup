9a2d49916c04af950b7e89bea0e2eb18daddbd44
will c <will.cra@outlook.com>
2020-02-22 13:26:23 -0500

    Updated program with fixed varible names and comments

--- js/script.js
+++ js/script.js
@@ -1,32 +1,35 @@
+//prompt user for vactionType and groupSize
 let vacationType = prompt("What kind of vacation would you like to go on; musical, tropical, or adventurous");
 let groupSize = prompt("What is your group size?");
 
+//condtionals that sets destination based on user input 
 if (vacationType === "musical") {
-    var string1 = " in New Orleans";
+    var destination = " in New Orleans";
     vacationType = "muscial vacation ";
 } 
 else if (vacationType === "tropical") {
-    var string1 = " to a Beach Vacation in Mexico";
+    var destination = " to a Beach Vacation in Mexico";
     vacationType = "tropical vacation ";
 }
 else if (vacationType === "adventurous") {
-    var string1 = " Whitewater Rafting the Grand Canyon";
+    var destination = " Whitewater Rafting the Grand Canyon";
     vacationType = "adventurous vacation ";
 }
 
-
+//condtionals that sets travel method based on user input 
 if (groupSize >= 1 && groupSize <= 2) {
-    var string2 = " by First Class";
+    var travelMethod = " by First Class";
     groupSize = (" with " + groupSize + " people,");
 } 
 else if (groupSize >= 3 && groupSize <= 5) {
-    var string2 = " by Helicopter";
+    var travelMethod = " by Helicopter";
     groupSize = (" with " + groupSize + " people,");
 }
 else if (groupSize >= 6) {
-    var string2 = " by Charter";
+    var travelMethod = " by Charter";
     groupSize = (" with " + groupSize + " people,");
 }
 
-var result = vacationType.concat(groupSize, string1, string2);
+//results displayed to console
+var result = vacationType.concat(groupSize, destination, travelMethod);
 console.log("You will be going on a " + result);
